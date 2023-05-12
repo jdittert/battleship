d59bc9f2e0172040ceb426ff760aa96dc1f4d3cc
jdittert <jondittert@gmail.com>
2023-05-12 12:11:05 -0400

    Finish function for displaying game board

--- src/dom.js
+++ src/dom.js
@@ -1,13 +1,46 @@
 import './style.css'; 
 import Board from './factories/board';
 
-export default function test() {
+export default function displayBoard() {
     const main = document.getElementById('main-wrapper');
     
     const board = new Board;
 
     board.buildGrid();
+
+    const gameBoard = document.createElement('div');
+    gameBoard.classList.add('game-board');
+    main.appendChild(gameBoard);
+
+    const rowNames = document.createElement('div');
+    rowNames.classList.add('row-names');
+    gameBoard.appendChild(rowNames);
+
+    board.letters.forEach((letter) => {
+        const letterDiv = document.createElement('div');
+        letterDiv.innerText = letter.toUpperCase();
+        letterDiv.classList.add('label');
+        rowNames.appendChild(letterDiv);
+    });
+
+    const colNames = document.createElement('div');
+    colNames.classList.add('column-names');
+    gameBoard.appendChild(colNames);
+
+    board.numbers.forEach((number) => {
+        const numDiv = document.createElement('div');
+        numDiv.innerText = number;
+        numDiv.classList.add('label');
+        colNames.appendChild(numDiv);
+    })
+
+    const spacer = document.createElement('div');
+    spacer.classList.add('spacer');
+    gameBoard.appendChild(spacer);
     
+    const gridDiv = document.createElement('div');
+    gridDiv.classList.add('game-grid');
+    gameBoard.appendChild(gridDiv);
 
     let i;
     let j;
@@ -15,7 +48,9 @@ export default function test() {
         for (j = 1; j < 11; j++) {
             const square = document.createElement('div');
             square.innerText = `${board.letters[i]}${j}`;
-            main.appendChild(square);
+            square.classList.add('board-square');
+            square.setAttribute('data-coordinates', `${board.letters[i]}${j}`);
+            gridDiv.appendChild(square);
         }
     };
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import test from "./dom";
+import displayBoard from "./dom";
 
-test();
+displayBoard();
